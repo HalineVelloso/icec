b7fe86d163c2377734ab60cb6e4cc99e90fd5b4e
Donwaztok <igor.rmartins@outlook.com>
2020-08-24 19:51:24 -0300

    Ajustado Install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
+sudo apt install python3 python3-pip gunicorn
 pip3 install django whitenoise gunicorn django-bootstrap4 django-stdimage django-adminlte2 django-filter
-sudo apt install gunicorn
 python3 manage.py makemigrations
 python3 manage.py migrate
 python3 manage.py createsuperuser
